81e4fee9c7afe5165e3df16b0862ecec597949d9
HarleyTBS <37520082+HarleyTBS@users.noreply.github.com>
2018-03-18 23:54:59 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,16 +4,15 @@ const TOKEN = "NDI0OTY2OTkwODM5NTQ1ODY4.DZB-4w.xNtNuwSjXggWqcXokQ8axSPCV_c";
 
 var bot = new Discord.Client();
 
-client.on("ready", function(message) {
+bot.on("ready", function(message) {
     console.log("Ready To Go!");
-    client.user.setGame("made by: HarleyTBS");
+    bot.user.setGame("made by: HarleyTBS");
 });
-client.on("ready", function() {
+bot.on("ready", function() {
     console.log("ready")
-    client.user.setGame("made by: HarleyTBS");
+    bot.user.setGame("made by: HarleyTBS");
     if (message.content === "ip") {
         message.reply("IP = (Not Released");
     }
 });
-
-client.login{process.env.BOT_TOKEN};
+bot.login(TOKEN).catch(console.log);
